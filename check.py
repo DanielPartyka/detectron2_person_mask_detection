80ba4f9d3dc1a2025d326908925ae8c060028d87
DanielPartyka <37247180+DanielPartyka@users.noreply.github.com>
2022-04-17 03:20:19 +0200

    Update check.py

--- check.py
+++ check.py
@@ -8,6 +8,7 @@ import numpy as np
 import os, json, cv2, random
 import sys
 import datetime
+import random
 from pathlib import Path
 # import some common detectron2 utilities
 from detectron2 import model_zoo
@@ -26,7 +27,7 @@ from detectron2.utils.visualizer import ColorMode
 
 from detectron2.engine import DefaultTrainer
 from detectron2.data.datasets import register_coco_instances
-register_coco_instances('p_dataset', {}, 'people_dataset/plik.json', 'people_dataset/images')
+register_coco_instances('p_dataset', {}, 'datasets/people_dataset/plik.json', 'datasets/people_dataset/images')
 
 fruits_nuts_metadata = MetadataCatalog.get("p_dataset")
 
@@ -53,16 +54,18 @@ image_folder_location = sys.argv[1]
 path = os.path.realpath(__file__)
 file_path = os.path.split(path)
 f = file_path[0]
-file_path = file_path[0] + '/' + str(sys.argv[1])
+file_path = file_path[0] + '\\' + str(sys.argv[1])
 counter = 0
 date = str(datetime.datetime.now())
-output_folder_path = f + '/' + date
+date.replace(" ", "")
+x = random.randint(1,20)
+output_folder_path = f + '\\' + str(x)
 os.mkdir(output_folder_path)
 
 try:
 
 	for image in os.listdir(file_path):
-		im = cv2.imread(str(sys.argv[1]) + '/' + image)
+		im = cv2.imread(str(sys.argv[1]) + '\\' + image)
 		height, width, channels = im.shape
 		outputs = predictor(im)
 		v = Visualizer(im[:, :, ::-1],
@@ -75,12 +78,12 @@ try:
 		try:
 		  plt.imshow(outputs["instances"].to("cpu").pred_masks.numpy().reshape(height, width), cmap='gray', aspect='auto')
 		  plt.axis('off')
-		  plt.savefig(output_folder_path + '/' + image, bbox_inches='tight', pad_inches=0)
+		  plt.savefig(output_folder_path + '\\' + image, bbox_inches='tight', pad_inches=0)
 		except:
 		  if counter == 0:
-		  	Path(output_folder_path + '/' + 'uwagi.txt').touch()
-		  file1 = open(output_folder_path + '/' + 'uwagi.txt', 'a')
-		  file1.write('Nie udalo sie utworzyc maski ze zdjecia ' + image + '\n')
+		  	Path(output_folder_path + '\\' + 'uwagi.txt').touch()
+		  file1 = open(output_folder_path + '\\' + 'uwagi.txt', 'a')
+		  file1.write('Nie udalo sie utworzyc maski ze zdjecia' + image + '\n')
 		  file1.close()
 		  counter += 1
   
@@ -88,6 +91,10 @@ except:
 	print("Wystapil problem z wczytywaniem zdjec")   
   
 print('Zapisano zdjecia z maskami do folderu: ', date)
+
+
+
+
 
 
 
